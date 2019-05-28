f9964df58d4ee7b21b532e531c4e650e0f5dbb40
WebMats <lorlando127@gmail.com>
2019-05-28 19:29:41 -0400

    fixed wrong variable call

--- src/14_cal.py
+++ src/14_cal.py
@@ -27,20 +27,15 @@ user_input = input('type month and year ')
 
 li = user_input.split(' ')
 
+date = datetime.now().strftime('%Y-%m')
+dateli = date.split('-')
+curcal = calendar.TextCalendar()
+
 if len(li) == 1 and len(li[0]) == 0:
-  date = datetime.now().strftime('%Y-%m')
-  dateli = date.split('-')
-  curcal = calendar.TextCalendar()
   print(curcal.formatmonth(int(dateli[0]), int(dateli[1]) ))
 elif len(li) == 1 and len(li[0]) > 0:
-  date = datetime.now().strftime('%Y-%m')
-  dateli = date.split('-')
-  curcal = calendar.TextCalendar()
   print(curcal.formatmonth(int(dateli[0]), int(user_input[0]) ))
 elif len(li) == 2:
-  date = datetime.now().strftime('%Y-%m')
-  dateli = date.split('-')
-  curcal = calendar.TextCalendar()
-  print(curcal.formatmonth(int(user_input[0]), int(user_input[1]) ))
+  print(curcal.formatmonth(int(li[1]), int(li[0]) ))
 else:
   print('Wrong format')
